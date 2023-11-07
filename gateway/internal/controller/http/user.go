fac9471667c5dedb92fd82a377c8353d699d6a4b
Evgenii <evgkhm@mail.ru>
2023-11-07 09:12:44 +0300

    add check and info for errors

--- gateway/internal/controller/http/user.go
+++ gateway/internal/controller/http/user.go
@@ -12,19 +12,20 @@ import (
 var (
 	ErrInternalServer = errors.New("InternalServerError")
 	ErrBadRequest     = errors.New("Bad request")
+	ErrEmptyCodeParam = errors.New("Empty code param")
 )
 
 func (h Handler) createUser(c *gin.Context) {
 	var userDTO *user.User
 	err := c.BindJSON(&userDTO)
 	if err != nil {
-		h.logger.Error("Bad request", ErrBadRequest.Error())
+		h.logger.Error("Error while bind userDTO to JSON", ErrBadRequest.Error())
 		c.JSON(http.StatusBadRequest, ErrBadRequest.Error())
 		return
 	}
 	err = h.natsSubscriber.PublishMessage(h.js, userDTO, nats.Config.Topic, "Create user")
 	if err != nil {
-		h.logger.Error("Internal server error", ErrInternalServer.Error())
+		h.logger.Error("Error while publish message to nats", ErrInternalServer.Error())
 		c.JSON(http.StatusInternalServerError, ErrInternalServer.Error())
 		return
 	}
@@ -35,17 +36,22 @@ func (h Handler) createUser(c *gin.Context) {
 func (h Handler) getBalanceUserByID(c *gin.Context) {
 	var err error
 	id := c.Param("id")
+	if id == "" {
+		h.logger.Error("Error while parse id", ErrEmptyCodeParam.Error())
+		c.JSON(http.StatusBadRequest, ErrEmptyCodeParam.Error())
+	}
+
 	var userDTO user.User
 	userDTO.ID, err = strconv.ParseUint(id, 10, 64)
 	if err != nil {
-		h.logger.Error("Internal server error", ErrInternalServer.Error())
+		h.logger.Error("Error while parse userDTO.ID", ErrInternalServer.Error())
 		c.JSON(http.StatusInternalServerError, ErrInternalServer.Error())
 		return
 	}
 
 	err = h.natsSubscriber.PublishMessage(h.js, &userDTO, nats.Config.Topic, "Get user balance")
 	if err != nil {
-		h.logger.Error("Internal server error", ErrInternalServer.Error())
+		h.logger.Error("Error while publish message to nats", ErrInternalServer.Error())
 		c.JSON(http.StatusInternalServerError, ErrInternalServer.Error())
 		return
 	}
@@ -57,14 +63,14 @@ func (h Handler) accrualBalanceUser(c *gin.Context) {
 	var userDTO *user.User
 	err := c.BindJSON(&userDTO)
 	if err != nil {
-		h.logger.Error("Bad request", ErrBadRequest.Error())
+		h.logger.Error("Error while bind userDTO to JSON", ErrBadRequest.Error())
 		c.JSON(http.StatusBadRequest, "Bad request")
 		return
 	}
 
 	err = h.natsSubscriber.PublishMessage(h.js, userDTO, nats.Config.Topic, "Accrual user balance")
 	if err != nil {
-		h.logger.Error("Internal server error", ErrInternalServer.Error())
+		h.logger.Error("Error while publish message to nats", ErrInternalServer.Error())
 		c.JSON(http.StatusInternalServerError, ErrInternalServer.Error())
 		return
 	}

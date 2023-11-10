49e4d036bc4b7651ddf6d8be7f9bfcf4e233c620
Evgenii <evgkhm@mail.ru>
2023-11-10 15:45:51 +0300

    interface for transactions and mocks added

--- consumer/internal/usecase/usecase.go
+++ consumer/internal/usecase/usecase.go
@@ -12,14 +12,17 @@ type UseCase struct {
 	User
 }
 
+//go:generate go run github.com/vektra/mockery/v2@v2.36.1 --name=User
 type User interface {
 	CreateUser(ctx context.Context, userDTO *user.User) error
 	GetBalance(ctx context.Context, id string) (user.User, error)
 	AccrualBalanceUser(ctx context.Context, userDTO *user.User) error
+	CheckNegativeBalance(ctx context.Context, userDTO *user.User) error
+	CalcNewBalance(ctx context.Context, userDTO *user.User, balance float32) float32
 }
 
-func New(repo *postgres.Repository, txService *transactions.TransactionServiceImpl, logger *slog.Logger) *UseCase {
+func New(repo *postgres.Repository, txService *transactions.Transactions, logger *slog.Logger) *UseCase {
 	return &UseCase{
-		User: NewUserUseCase(repo.UserRepository, txService, logger),
+		User: NewUserUseCase(repo.UserRepository, txService.Transactor, logger),
 	}
 }
